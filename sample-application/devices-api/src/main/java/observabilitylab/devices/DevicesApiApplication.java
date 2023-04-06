22c400d389166b275d9bb6f9a4853f24243bd3f8
Izabela Kulakowska <ikulakowska@microsoft.com>
2023-04-06 14:04:51 +0200

    changes

--- sample-application/devices-api/src/main/java/observabilitylab/devices/DevicesApiApplication.java
+++ sample-application/devices-api/src/main/java/observabilitylab/devices/DevicesApiApplication.java
@@ -1,6 +1,5 @@
 package observabilitylab.devices;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
--- sample-application/devices-api/src/main/java/observabilitylab/devices/api/DevicesController.java
+++ sample-application/devices-api/src/main/java/observabilitylab/devices/api/DevicesController.java
@@ -7,7 +7,6 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
