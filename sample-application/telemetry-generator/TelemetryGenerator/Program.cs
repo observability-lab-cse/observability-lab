b732248eefd20d7384d6653f741a9b6cdd0594e4
Maggie Salak <masalak@microsoft.com>
2023-05-17 11:37:38 +0200

    rename the .NET app

---
+++ sample-application/telemetry-generator/TelemetryGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TelemetryGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Telemetry generator started.");
+        }
+    }
+}
