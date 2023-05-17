f8ee90dc9ba37042eb790a3d639f3c90dc7824b3
Maggie Salak <masalak@microsoft.com>
2023-05-17 15:05:31 +0200

    add a mqtt message publisher

---
+++ sample-application/telemetry-generator/TelemetryGenerator/IMessagePublisher.cs
@@ -0,0 +1,15 @@
+﻿namespace TelemetryGenerator
+{
+    /// <summary>
+    /// Exposes the functionality of publishing messages with MQTT.
+    /// </summary>
+    public interface IMessagePublisher
+    {
+        /// <summary>
+        /// Publishes an MQTT message.
+        /// </summary>
+        /// <param name="message">The message to publish.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        public Task PublishMessage(string message, CancellationToken cancellationToken);
+    }
+}
