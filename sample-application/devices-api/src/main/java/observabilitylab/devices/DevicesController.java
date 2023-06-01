c54445753eb965f86f1f74053ac27783cf8588ea
Maggie Salak <masalak@microsoft.com>
2023-06-01 17:09:11 +0200

    return response status code 201

--- sample-application/devices-api/src/main/java/observabilitylab/devices/DevicesController.java
+++ sample-application/devices-api/src/main/java/observabilitylab/devices/DevicesController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -26,9 +27,9 @@ public class DevicesController {
     }
 
     @PostMapping
-    public ResponseEntity<Device> addDevice(@RequestBody Device device) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public Device addDevice(@RequestBody Device device) {
         var newDevice = new Device(device.getId());
-        repository.save(newDevice);
-        return new ResponseEntity<>(newDevice, HttpStatus.CREATED);
+        return repository.save(newDevice);
     }
 }
