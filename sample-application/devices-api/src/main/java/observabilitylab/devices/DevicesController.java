4d569d4130cf350d32b4db9197aaf5be826be044
Tamara Rahimi <mahra.rah@gmail.com>
2023-06-05 19:11:37 +0000

    Add Device deletion and get endpoints

--- sample-application/devices-api/src/main/java/observabilitylab/devices/DevicesController.java
+++ sample-application/devices-api/src/main/java/observabilitylab/devices/DevicesController.java
@@ -1,18 +1,20 @@
 package observabilitylab.devices;
 
+import java.util.List;
 import observabilitylab.devices.model.Device;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+
 
 @RestController
 @RequestMapping(path = "/devices")
@@ -31,5 +33,24 @@ public class DevicesController {
     public Device addDevice(@RequestBody Device device) {
         var newDevice = new Device(device.getName());
         return repository.save(newDevice);
+    }
+    
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Device getDeviceById(@PathVariable("id") String id){
+        return repository.getDeviceById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteDeviceById(@PathVariable("id") String id){
+        repository.deleteDeviceById(id);
+    }
+
+    @PutMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public Device updatDevice(@RequestBody Device device){
+        // TODO: Implement update device
+        return repository.getDeviceById(device.getId());
     }
 }
--- sample-application/devices-api/src/main/java/observabilitylab/devices/DevicesRepository.java
+++ sample-application/devices-api/src/main/java/observabilitylab/devices/DevicesRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Repository;
 
 import com.azure.spring.data.cosmos.repository.CosmosRepository;
+import com.azure.spring.data.cosmos.repository.Query;
 
 import observabilitylab.devices.model.Device;
 
@@ -12,4 +13,8 @@ import observabilitylab.devices.model.Device;
 public interface DevicesRepository extends CosmosRepository<Device, String> {
     
     List<Device> findAll();
+    
+    Device getDeviceById(String deviceId);
+    void deleteDeviceById(String deviceId);
+    // Device updateDevice(String deviceId, Updat);
 }
