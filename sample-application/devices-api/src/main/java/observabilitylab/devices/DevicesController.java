301ed164b2d370ffa340779dfdaa9ad51eee1bdf
Tamara Rahimi <mahra.rah@gmail.com>
2023-07-17 09:45:19 +0000

    Combin update and create in one endpoint for now

--- sample-application/devices-api/src/main/java/observabilitylab/devices/DevicesController.java
+++ sample-application/devices-api/src/main/java/observabilitylab/devices/DevicesController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,10 +28,9 @@ public class DevicesController {
         return repository.findAll();
     }
 
-    @PostMapping
+    @PutMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Device addDevice(@RequestBody Device device) {
-        //TODO: do we want to pass in ID? or do we want a CreateDevice Object > will effect what CRUD we need
+    public Device addOrUpdateDevice(@RequestBody Device device) {
         return repository.save(device);
     }
     
@@ -47,12 +45,5 @@ public class DevicesController {
     public void deleteDeviceById(@PathVariable("id") String id){
         // log.info("Device with id {id} was deleted")
         repository.deleteDeviceById(id);
-    }
-
-    @PutMapping()
-    @ResponseStatus(HttpStatus.OK)
-    public Device updatDevice(@RequestBody Device device){
-        // log.info("Device with id {id} was updated")
-        return repository.save(device);
     }
 }
