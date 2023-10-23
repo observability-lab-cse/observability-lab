cbc7512a7dcb86dab33b10d35e1002ad08a04595
Magdalena Baran <mbaran@microsoft.com>
2023-10-23 21:31:21 +0000

    Use Host default builder and implement EventHubService as IHostedService

--- sample-application/device-manager/DeviceManager/Program.cs
+++ sample-application/device-manager/DeviceManager/Program.cs
@@ -1,53 +1,33 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using DeviceManager;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Configuration.Json;
 
+using var host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    var configuration = hostContext.Configuration;
+                    var consumerGroup = configuration.GetValue<string>("CONSUMER_GROUP");
+                    var blobContainerName = configuration.GetValue<string>("BLOB_CONTAINER_NAME");
+                    var storageConnectionString = configuration.GetValue<string>("STORAGE_CONNECTION_STRING");
+                    var eventHubConnectionString = configuration.GetValue<string>("EVENT_HUB_CONNECTION_STRING");
+                    var eventHubName = configuration.GetValue<string>("EVENT_HUB_NAME");
+                    var deviceApiUrl = configuration.GetValue<string>("DEVICE_API_URL");
 
-namespace DeviceManager
-{
-    internal class Program
-    {
-        static async Task Main(string[] args)
-        {
-            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json",true,true)
-                                                        .AddEnvironmentVariables()
-                                                        .Build();
-            var consumerGroup = configuration.GetValue<string>("CONSUMER_GROUP");
-            var blobContainerName = configuration.GetValue<string>("BLOB_CONTAINER_NAME");
-            var storageConnectionString = configuration.GetValue<string>("STORAGE_CONNECTION_STRING"); 
-            var eventHubConnectionString = configuration.GetValue<string>("EVENT_HUB_CONNECTION_STRING");
-            var eventHubName = configuration.GetValue<string>("EVENT_HUB_NAME");
-            var deviceApiUrl = configuration.GetValue<string>("DEVICE_API_URL");
+                    services.AddHostedService(provider =>
+                    {
+                        var logger = provider.GetRequiredService<ILogger<EventHubReceiverService>>();
+                        return new EventHubReceiverService(
+                            storageConnectionString,
+                            blobContainerName,
+                            eventHubConnectionString,
+                            eventHubName,
+                            consumerGroup,
+                            deviceApiUrl,
+                            logger);
+                    });
+                })
+                .Build();
 
-            var serviceCollection = new ServiceCollection();
-            serviceCollection.AddLogging(builder =>
-            {
-                builder.AddConsole();
-            });
-            serviceCollection.AddSingleton<EventHubReceiverService>(provider =>
-            {
-                var logger = provider.GetRequiredService<ILogger<EventHubReceiverService>>();
-                return new EventHubReceiverService(
-                    storageConnectionString,
-                    blobContainerName,
-                    eventHubConnectionString,
-                    eventHubName,
-                    consumerGroup,
-                    deviceApiUrl,
-                    logger);
-            });
-
-            var serviceProvider = serviceCollection.BuildServiceProvider();
-
-            var eventHubReceiverService = serviceProvider.GetRequiredService<EventHubReceiverService>();
-
-            await eventHubReceiverService.StartProcessingAsync();
-
-            await Task.Delay(TimeSpan.FromSeconds(300));
-
-            await eventHubReceiverService.StopProcessingAsync();
-        }
-
-    }
-}
+await host.RunAsync();
