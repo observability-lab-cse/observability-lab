82344bb1c537f598c22f89399d2a2dbbc908d817
Izabela Kulakowska <ikulakowska@microsoft.com>
2023-10-27 17:35:47 +0200

    Fix

--- sample-application/sample.sh
+++ sample-application/sample.sh
@@ -47,7 +47,6 @@ deploy(){
     sed -e "s/\${project-name}/$ENV_PROJECT_NAME/" | \
     kubectl apply -f  -
 
-    EVENT_HUB_CONNECTION_STRING=$(az eventhubs eventhub authorization-rule keys list --resource-group "$ENV_RESOURCE_GROUP_NAME" --namespace-name evhns-"$ENV_PROJECT_NAME" --eventhub-name evh-"$ENV_PROJECT_NAME" --name Listen  --query primaryConnectionString -o tsv)
     STORAGE_CONNECTION_STRING=$(az storage account show-connection-string --name st$ENV_PROJECT_NAME --resource-group $ENV_RESOURCE_GROUP_NAME -o tsv)
     
     cat k8s-files/device-manager-deployment.yaml | \
@@ -147,7 +146,6 @@ deploy_devices_simulator(){
     DEVICE_NAMES=$(curl -X GET --header 'Accept: application/json' "http://$DEVICES_API_IP:8080/devices" | jq -r '[.[].name] | join(",")')
     echo "Configuring the devices simulator for the following device names: $DEVICE_NAMES"
 
-    EVENT_HUB_CONNECTION_STRING=$(az eventhubs eventhub authorization-rule keys list --resource-group "$ENV_RESOURCE_GROUP_NAME" --namespace-name evhns-"$ENV_PROJECT_NAME" --eventhub-name evh-"$ENV_PROJECT_NAME" --name Send --query primaryConnectionString -o tsv)
     cat k8s-files/devices-simulator-deployment.yaml | \
     sed -e "s#DEVICE_NAMES_PLACEHOLDER#$DEVICE_NAMES#" | \
     kubectl apply -f  -
