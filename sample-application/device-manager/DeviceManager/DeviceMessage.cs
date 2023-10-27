3f77d93af9e0cbc462f1752956b6bea21b579b04
Tamara Rahimi <mahra.rah@gmail.com>
2023-10-27 15:15:36 +0200

    Remove k8s and sample files

--- sample-application/device-manager/DeviceManager/DeviceMessage.cs
+++
@@ -1,9 +0,0 @@
-namespace DeviceManager
-{
-    public class DeviceMessage
-    {
-        public string? deviceId { get; set; }
-        public string? deviceTimestamp { get; set; }
-        public float temp { get; set; }
-    }
-}
--- sample-application/devices-api/src/main/java/observabilitylab/devices/model/DeviceStatus.java
+++
@@ -1,7 +0,0 @@
-package observabilitylab.devices.model;
-
-   public enum DeviceStatus {
-        NEW,
-        IN_USE,
-        ERROR
-    }
